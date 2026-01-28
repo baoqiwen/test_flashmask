697e6f1f9f08f362298a8f4356cdd1708e246e75
umiswing <umiswing@foxmail.com>
2026-01-28 14:48:20 +0800

    add fa4 to benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -6,5 +6,8 @@ python benchmark_flashmask.py --fm_version 4  --suffix ""
 sleep 60
 python benchmark_flexattention.py
 sleep 60
+python benchmark_fa4_mask_mod.py
+sleep 60
 python draw.py
 python draw.py --baseline "flexattention"
+python plot_radar.py --methods 'fa4_mask_mod' 'flashmaskv4'
